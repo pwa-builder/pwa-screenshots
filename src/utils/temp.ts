a4bd090581469b48aa55b84f86b6514c64eb92c9
lee-leonardo <lee.leonardo.king@gmail.com>
2021-03-29 17:55:35 -0700

    temp

---
+++ src/utils/temp.ts
@@ -0,0 +1,57 @@
+import tmp, { dir } from 'tmp';
+import del from 'del';
+
+tmp.setGracefulCleanup();
+
+const timeout = 1000 * 60 * 5; // 5 minutes
+
+export async function createTemporaryFile(filename: string, extension: string): Promise<string | undefined> {
+  try {
+    const prefix = normalizePath(filename);
+    return tmp.tmpNameSync({
+      prefix,
+      postfix: extension,
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function createTemporaryDir(dirName: string): Promise<string | undefined> {
+  try {
+    const prefix = normalizePath(dirName);
+    const result = tmp.dirSync({
+      prefix
+    });
+
+    return result.name;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function scheduleTemporaryFileDeletion(tmpName: string | undefined) {
+  try {
+    if (tmpName) {
+      const fn = () => del([normalizePath(tmpName)]);
+      setTimeout(fn, timeout);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function scheduleTemporaryDirDeletion(dirPath: string | undefined) {
+  try {
+    if (dirPath) {
+      const fn = () => del([normalizePath(dirPath)], { force: true });
+      setTimeout(fn, timeout);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function normalizePath(path: string): string {
+  return path.replace(/\\/g, "/");
+}
